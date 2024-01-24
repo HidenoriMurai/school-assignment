6474c4bbc4d4fca559800496be4e65314bcba351
Hidenori Murai <hidenoweb822@gmail.com>
2024-01-24 22:58:52 +0900

    プロジェクト作成

---
+++ src/test/java/com/assignment/th/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
