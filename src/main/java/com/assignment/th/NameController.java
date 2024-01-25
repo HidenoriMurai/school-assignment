79108b28939c251927763b37e92a8bd0eeaeab95
Hidenori Murai <hidenoweb822@gmail.com>
2024-01-25 18:17:41 +0900

    テーブルからレコードを全件取得するAPIの実装を追加

---
+++ src/main/java/com/assignment/th/NameController.java
@@ -0,0 +1,21 @@
+package com.assignment.th;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NameController {
+    private NameMapper nameMapper;
+
+    public NameController(NameMapper nameMapper) {
+        this.nameMapper = nameMapper;
+    }
+
+    @GetMapping("/names")
+    public List<Name> getAll() {
+        return nameMapper.getAll();
+    }
+
+}
