8a31dc7e5eff307a0dd7af5f563655e9733b3953
Hidenori Murai <hidenoweb822@gmail.com>
2024-01-28 17:54:32 +0900

    例外ハンドリングする処理を追加

--- src/main/java/com/assignment/th/NameController.java
+++ src/main/java/com/assignment/th/NameController.java
@@ -1,20 +1,43 @@
 package com.assignment.th;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class NameController {
-    private final NameService nameServiceer;
+    private final NameService nameService;
 
-    public NameController(NameService nameServiceer) {
-        this.nameServiceer = nameServiceer;
+    public NameController(NameService nameService) {
+        this.nameService = nameService;
     }
 
     @GetMapping("/names")
-    public List<Name> getByNames() {
-        return nameServiceer.getAll();
+    public List<Name> getByNames(@RequestParam(required = false) String startsWith) {
+        if (startsWith != null) {
+            return nameService.getByNameStartsWith(startsWith);
+        } else {
+            return nameService.getAll();
+        }
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleUserNotFoundException(
+            UserNotFoundException e, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/assignment/th/NameService.java
+++ src/main/java/com/assignment/th/NameService.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Service
 public class NameService {
 
-    private NameMapper nameMapper;
+    private final NameMapper nameMapper;
 
     public NameService(NameMapper nameMapper) {
         this.nameMapper = nameMapper;
@@ -15,5 +15,14 @@ public class NameService {
 
     public List<Name> getAll() {
         return nameMapper.getAll();
+    }
+
+    public List<Name> getByNameStartsWith(String startsWith) {
+        List<Name> result = nameMapper.getByNameStartsWith(startsWith);
+        if (result.isEmpty()) {
+            throw new UserNotFoundException(startsWith + " で始まる名前は存在しません。");
+        } else {
+            return nameMapper.getByNameStartsWith(startsWith);
+        }
     }
 }
